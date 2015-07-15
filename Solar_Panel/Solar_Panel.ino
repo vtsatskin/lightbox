6bcaccc39d173f3f7cebe7d58bf5beaf1c060040
WBradbeer <willbradbeer@gmail.com>
2015-07-15 17:04:49 -0400

    Solar Panel Working

--- Solar_Panel/Solar_Panel.ino
+++ Solar_Panel/Solar_Panel.ino
@@ -1,12 +1,12 @@
 const int firstPin = A0;
 const int secondPin = A1;
-const int smallR = 270;
-const int bigR = 27000;
+const double smallR = 1.8;
+const int bigR = 18;
 
 void setup() {
   // put your setup code here, to run once:
   
-  analogReference(EXTERNAL);
+  //analogReference(EXTERNAL);
   Serial.begin(9600);
 
 }
@@ -15,18 +15,26 @@ void loop() {
   // put your main code here, to run repeatedly:
 int firstVal = analogRead(firstPin);
 int secondVal = analogRead(secondPin);
-float firstVolt = (firstVal/1024.0)*3.3;
-float secondVolt = (secondVal/1024.0)*3.3;
+float firstVolt = (firstVal/1024.0)*5;
+float secondVolt = (secondVal/1024.0)*5;
 double current = (firstVolt - secondVolt)/smallR;
-double power = current*current*bigR;
+double power = current*current*(bigR+smallR);
 Serial.print(firstVal);
 Serial.print(", ");
 Serial.print(secondVal);
 Serial.print(", ");
+Serial.print(firstVolt);
+Serial.print(", ");
+Serial.print(secondVolt);
+Serial.print(", ");
+Serial.print(firstVolt - secondVolt);
+Serial.print(", ");
+Serial.print(smallR);
+Serial.print(", ");
 Serial.print(current*1000,6);
 Serial.print("mA");
 Serial.print(" ");
-Serial.print(power*1000,6);
-Serial.print("mW \n");
+Serial.print(power,6);
+Serial.print("W \n");
 
 }
