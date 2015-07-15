2e7e60ecbdcb8c1a8afcd73623b15fb6561b9c3b
WBradbeer <willbradbeer@gmail.com>
2015-07-14 23:21:25 -0400

    Started data conversion

--- src/regression.py
+++ src/regression.py
@@ -1,6 +1,10 @@
 import numpy as np
+import pandas as pd
 from sklearn import  linear_model
 
+import json
+
+##Test Regresion##
 x = np.array([[x,0] for x in range(20)])
 y = np.array([[y*2] for y in range(20)])
 print(len(x))
@@ -10,3 +14,11 @@ regr.fit(x,y)
 print (regr.coef_)
 print(regr.intercept_)
 
+##Data Conversion
+text_file = open("../data/hour.txt", 'r')
+minutely = json.loads(text_file.read())
+total = 0
+for minute in minutely:
+	total = total + minute['values']
+average = total/(len(minutely))
+print(average)
