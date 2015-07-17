9816661878a20e1b936598f47b288f53c1030f05
Valentin Tsatskin <val@valtsatskin.com>
2015-07-17 16:41:50 -0400

    improved streaming code
    * whitespace fixes
    * removed dead code
    * ensure current is not negative
    * removed “solar” from keys
    * changed current to mA
    * removed error strings
    * stream rate increased to 125ms

--- Arduino/Combination_Readings/Combination_Readings.ino
+++ Arduino/Combination_Readings/Combination_Readings.ino
@@ -1,13 +1,8 @@
 #include <Wire.h>
-
 #include <Adafruit_10DOF.h>
-
 #include <Adafruit_Sensor.h>
-
 #include <Adafruit_L3GD20_U.h>
-
 #include <Adafruit_LSM303_U.h>
-
 #include <Adafruit_BMP085_U.h>
 
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
@@ -19,37 +14,16 @@ const int secondPin = A1;
 const double smallR = 1.8;
 const int bigR = 18;
 
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  accel.getSensor(&sensor);
-  /*Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");  
-  Serial.println("------------------------------------");
-  Serial.println("");*/
-  delay(500);
-}
-
 void setup(void) {
   Serial.begin(9600);
-  /*Serial.println("Sensor Test");
-  Serial.println("");*/
-  
-  displaySensorDetails();
 
-  if (!bmp.begin() || !mag.begin() || !accel.begin())
-  {
+  if (!bmp.begin() || !mag.begin() || !accel.begin()) {
     Serial.print("Ooops, no BMP085 or LSM303 detected");
     while(1);
   }
 }
+
 void loop() {
-  
   sensors_event_t eventP;
   sensors_event_t eventM;
   sensors_event_t eventA;
@@ -73,64 +47,42 @@ void loop() {
     heading = 360 + heading; 
   }
   Serial.print("{");
-  Serial.print("\"solarpanelcurrent\": ");  
-  Serial.print(current*1000,6);
+  
+  Serial.print("\"current\": ");  
+  Serial.print(current*1000);
   Serial.print(", ");
-  Serial.print("\"solarpanelpower\": ");  
+  
+  Serial.print("\"power\": ");  
   Serial.print(power,6);
   Serial.print(", ");
+  
   Serial.print("\"pressure\": ");
-  if (eventP.pressure)
-  {
-    Serial.print(eventP.pressure);
-  }
-  else
-  {
-    Serial.print("\"error\"");
-  }
-    Serial.print(", ");
-  if (temperature)
-  {
-    Serial.print("\"temperature\": ");
-    Serial.print(temperature);
-  }
-  else
-  {
-    Serial.println("\"error\"");
-  }
+  Serial.print(eventP.pressure);
   Serial.print(", ");
+  
+  Serial.print("\"temperature\": ");
+  Serial.print(temperature);
+  Serial.print(", ");
+  
   Serial.print("\"compass\": ");
-   if (heading)
-  {
-    Serial.print("[");
-    Serial.print(eventM.magnetic.y);
-    Serial.print(", ");
-    Serial.print(eventM.magnetic.x);
-    Serial.print(", ");
-    Serial.print(heading);
-    Serial.print("] ");
-  }
-  else
-  {
-    Serial.print("error");
-  }
+  Serial.print("[");
+  Serial.print(eventM.magnetic.y);
+  Serial.print(", ");
+  Serial.print(eventM.magnetic.x);
+  Serial.print(", ");
+  Serial.print(heading);
+  Serial.print("]");
   Serial.print(", ");
   Serial.print("\"accelerometer\": ");
-  if (1)
-  {
-    Serial.print("[");
-    Serial.print(eventA.acceleration.x);
-    Serial.print(",");
-    Serial.print(eventA.acceleration.y);
-    Serial.print(",");
-    Serial.print(eventA.acceleration.z);
-    Serial.print("]");
-  }
-  else
-  {
-    Serial.print("\"error\"");
-  }
-  Serial.print("}");  
-   delay(1000);
+  Serial.print("[");
+  Serial.print(eventA.acceleration.x);
+  Serial.print(",");
+  Serial.print(eventA.acceleration.y);
+  Serial.print(",");
+  Serial.print(eventA.acceleration.z);
+  Serial.print("]");
+
+  Serial.print("}\n");  
   
+  delay(125);
 }
