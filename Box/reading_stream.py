d33ff4fb133e2b326af7de93fecb8af5c5411e3a
Valentin Tsatskin <val@valtsatskin.com>
2015-07-17 18:42:46 -0400

    init real-time graph

--- Box/reading_stream.py
+++ Box/reading_stream.py
@@ -1,9 +1,38 @@
-import serial
 import logging
 import sys
+import serial
 import json
 import couchdb
 import time
+import datetime
+import plotly.plotly as py
+from plotly.graph_objs import Scatter, Layout, Figure
+
+username = ''
+api_key = ''
+stream_token = ''
+
+py.sign_in(username, api_key)
+
+trace1 = Scatter(
+    x=[],
+    y=[],
+    stream=dict(
+        token=stream_token,
+        maxpoints=200
+    )
+)
+
+stream = py.Stream(stream_token)
+stream.open()
+
+layout = Layout(
+    title='Raspberry Pi Streaming Sensor Data'
+)
+
+fig = Figure(data=[trace1], layout=layout)
+
+print py.plot(fig, filename='Raspberry Pi Streaming Example Values')
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
@@ -32,3 +61,5 @@ while True:
     logging.debug("JSON reading: %s", data)
 
     readings.save(data)
+
+    stream.write({'x': datetime.datetime.now(), 'y': data['accelerometer'][0]})
